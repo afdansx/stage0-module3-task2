d50ee66e34e3d15f75dbd8238b7d78fac2bc3cce
daniel <dd.naidenko@gmail.com>
2023-10-27 23:22:33 +0300

    solved

--- src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
@@ -2,13 +2,13 @@ package school.mjc.stage0.module3.task2;
 
 public class JavaVariableNames {
     public static void main(String[] args) {
-        int 2morrow = 0;
-        int $money = 10;
-        int %hundred = 100;
-        int !back = 1000;
-        int _underscore = 10000;
-        int #sharp = 100000;
-        int *star = 1000000;
-        int BADEXAMPLEOFVARNAME = 1000000;
+        int var1 = 0;
+        int var2 = 10;
+        int var3 = 100;
+        int var4 = 1000;
+        int var5 = 10000;
+        int var6 = 100000;
+        int var7 = 1000000;
+        int var8 = 1000000;
     }
 }
